7a89cfbe85cd4fd08578993fc771c8bf9ec9c646
iamjerry07 <rohansingh8382@gmail.com>
2022-05-22 21:52:57 +0530

    done

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -69,39 +69,6 @@ const createUrl = async (req, res) => {
     }
 }
 
-// const postUrl = async function (req, res) {
-
-//     try {
-//         let userData = req.body.longUrl
-//         let user = req.body
-
-//             if (!(Object.keys(user).length || user == null || user == undefined)) {
-//                 return res.status(400).send({ status: false, message: "Type a key==> (longUrl) and value ==> (your URL) to shorten" })
-//             }
-//             // if(!userData){
-//             //    return res.status(400).send( {status: false, message: "Give any url to shorten"})
-//             // }
-//             if (!(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(userData))) {
-//                 return res.status(400).send({ status: false, message: "Give a valid url" })
-//             }
-//             // if(!(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/.test(userData))){
-//             //     return res.status(400).send( {status: false, message: "Give a valid url"})
-//             // }
-
-//             let shortID = (shortid.generate(userData)).toLowerCase()
-//             let short = "http://localhost:3000/" + shortID
-
-//             let obj = { longUrl: userData, shortUrl: short, urlCode: shortID }
-
-//             let createdUrl = await urlModel.create(obj)
-//             return res.status(201).send({ status: true, data: createdUrl })
-//         } 
-// catch (error) {
-//             return res.status(500).send({ status: false, message: error.message })
-//         }
-//     }
-
-
 
 const getUrl = async function (req, res) {
     let data = req.params.urlCode
